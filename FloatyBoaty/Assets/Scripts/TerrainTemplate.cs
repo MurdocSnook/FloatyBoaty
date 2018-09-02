3ef7dce9be0eae4f0e7443c66aa074c6143e5d87
Nils Gawlik <nilsgawlik@gmx.de>
2018-09-02 20:11:11 +0200

    Add curve for currents in remplates

--- FloatyBoaty/Assets/Scripts/TerrainTemplate.cs
+++ FloatyBoaty/Assets/Scripts/TerrainTemplate.cs
@@ -1,19 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EasyButtons;
 
+[ExecuteInEditMode]
 public class TerrainTemplate : MonoBehaviour {
 	public GameObject entranceMarker;
 	public GameObject exitMarker;
 
+	[Header("Water current curve parameters")]
+	public float smoothness = 1f;
+	public int iterationSteps = 3;
+
+	private List<Vector3> curve;
+
 	// Use this for initialization
 	void Start () {
-		
+		CalulateCurve();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		#if UNITY_EDITOR
+			CalulateCurve();
+		#endif
+	}
+
+	void CalulateCurve() {
+		// use subdivisions to smooth out the curve
+		curve = new List<Vector3>();
+		float offset = smoothness * 0.333f * Vector3.Distance(entranceMarker.transform.position, exitMarker.transform.position);
+		// first 4 points, the two middle points change depending on the smoothness
+		curve.Add(entranceMarker.transform.position);
+		curve.Add(entranceMarker.transform.position + entranceMarker.transform.right * offset);
+		curve.Add(exitMarker.transform.position - exitMarker.transform.right * offset);
+		curve.Add(exitMarker.transform.position);
+
+		// iteratively smooth, doubling the points each step
+		for(int i = 0; i < iterationSteps; i++) {
+			List<Vector3> newCurve = new List<Vector3>();
+
+			newCurve.Add(curve[0]);
+			for(int j = 0; j < curve.Count - 1; j++) {
+				// We add two intermediary points per segment, but don't re-add the existing points
+				// We leave out a point for the last and first segment, because they are not necesessary there
+				if(j != 0)
+					newCurve.Add(Vector3.Lerp(curve[j], curve[j+1], 0.25f));
+				if(j != curve.Count - 2)
+					newCurve.Add(Vector3.Lerp(curve[j], curve[j+1], 0.75f));
+			}
+			newCurve.Add(curve[curve.Count - 1]);
+			curve = newCurve;
+		}
+	}
+
+	private void OnDrawGizmos() {
+		if(curve != null) {
+			foreach (Vector3 pos in curve) {
+				Gizmos.DrawSphere(pos, 1);
+			}
+		}
 	}
 
 	private void OnValidate() {
