e06518042ef212b8f7c9e63c53612a7af0f49a23
Nils Gawlik <nilsgawlik@gmx.de>
2018-09-04 18:51:16 +0200

    Add a new force to steering to make the boat more controllable

--- FloatyBoaty/Assets/Scripts/RaftController.cs
+++ FloatyBoaty/Assets/Scripts/RaftController.cs
@@ -10,6 +10,7 @@ public class RaftController : MonoBehaviour {
 	public float baseWaterSpeed = 1f;
 	public float baseWaterForce = 1f;
 	public float rotationForce = 2f;
+	public float centreboardForce = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,19 +20,25 @@ public class RaftController : MonoBehaviour {
 	void FixedUpdate() {
 		Rigidbody rb = GetComponent<Rigidbody>();
 
-		Vector3 waterSpeed = riverGenerator.GetWaterDirectionAt(transform.position) * baseWaterSpeed;
+        Vector3 waterDir = riverGenerator.GetWaterDirectionAt(transform.position);
+        Vector3 waterSpeed = waterDir * baseWaterSpeed;
 
 		// apply water force
 		Vector3 relativeSpeed = (waterSpeed - rb.velocity) * baseWaterForce;
 		rb.AddForce(relativeSpeed);
 
-		// simulate rudder as if we were on a sail boat
-		// calculate water pushing against rudder
+		// calculate water pushing against boat (kind of fake, ignores that water is moving with boat)
 		Vector3 perceivedWaterVelocity = -rb.velocity;
-		// calculate thrust caused by the pushing water
-		Vector3 thrust = Vector3.Dot(rudder.transform.forward, perceivedWaterVelocity) * rudder.transform.forward;
+
+		// calculate thrust caused by the pushing water on the raft
+		Vector3 thrustRaft = Vector3.Dot(transform.forward, perceivedWaterVelocity) * transform.forward;
+		// apply thrust at centreboard
+		rb.AddForceAtPosition(thrustRaft * centreboardForce, transform.position);
+
+		// calculate thrust caused by the pushing water on the rudder
+		Vector3 thrustRudder = Vector3.Dot(rudder.transform.forward, perceivedWaterVelocity) * rudder.transform.forward;
 		// apply thrust at rudder
-		rb.AddForceAtPosition(thrust * rotationForce, rudder.transform.position);
+		rb.AddForceAtPosition(thrustRudder * rotationForce, rudder.transform.position);
 	}
 	
 	void Update () {
