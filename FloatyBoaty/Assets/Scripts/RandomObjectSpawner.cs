7f66b17cfebe7194ea15e70db969c5f157aac22a
MurdocSnook <murdoc.snook@gmx.de>
2018-09-08 22:44:05 +0200

    Fix mill & add object spawn script

---
+++ FloatyBoaty/Assets/Scripts/RandomObjectSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using EasyButtons;
+
+public class RandomObjectSpawner : MonoBehaviour
+{
+    public GameObject[] spawnObjects;
+    public GameObject[] spawnPoints;
+    public float RespawnTime = 5f;
+    private GameObject item;
+
+    void Start()
+    {
+        Spawn();
+    }
+    [Button]
+    private void Spawn()
+    {
+        int spawnObjectIndex = Random.Range(0, spawnObjects.Length);
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        item = GameObject.Instantiate(spawnObjects[spawnObjectIndex]) as GameObject;
+
+        item.transform.parent = spawnPoints[spawnPointIndex].transform;
+        item.transform.position = spawnPoints[spawnPointIndex].transform.position;
+        item.transform.rotation = spawnPoints[spawnPointIndex].transform.rotation;
+    }
+
+    public IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(RespawnTime);
+        Spawn();
+    }
+}
