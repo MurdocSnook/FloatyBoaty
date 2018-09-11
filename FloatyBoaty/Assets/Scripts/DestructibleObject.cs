e3d4f187cc31d0517447255862357d3e78738f00
Nils Gawlik <nilsgawlik@gmx.de>
2018-09-11 16:54:53 +0200

    Make Raft destructible

---
+++ FloatyBoaty/Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour {
+	public int hp = 100;
+	public GameObject[] damageStates;
+
+	private int maxHp;
+	private int currentDamageState;
+
+	private void Start() {
+		maxHp = hp;
+		currentDamageState = 0;
+	}
+
+	private void Update() {
+		if(damageStates != null && damageStates.Length > 0) {
+			int state = Mathf.Clamp(
+				(int)((1f - (float) hp / maxHp) * damageStates.Length), 
+				0, 
+				damageStates.Length-1
+			);
+
+			if(state != currentDamageState) {
+				ChangeState(state);
+			}
+		} 
+	}
+
+	private void ChangeState(int newState) {
+		damageStates[currentDamageState].SetActive(false);
+		damageStates[newState].SetActive(true);
+		currentDamageState = newState;
+	}
+
+	public void DealDamage(int damage) {
+		hp -= damage;
+	}
+}
