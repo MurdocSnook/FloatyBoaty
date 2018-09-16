42871074286f332bc686f6855d7239603926d6cb
Nils Gawlik <nilsgawlik@gmx.de>
2018-09-16 22:00:17 +0200

    Add deactivate on start component

---
+++ FloatyBoaty/Assets/Scripts/DeactivateOnStart.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateOnStart : MonoBehaviour {
+	private void Start() {
+		gameObject.SetActive(false);
+	}
+}
